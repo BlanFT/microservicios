1a9b70f21d75a3701287e8bcb0f250561eb48693
Brandon FT <Brandon_ft10@outlook.com>
2024-12-11 17:47:13 -0600

    Configuracion para la rama de Eureka

--- microservice-user/src/main/java/com/microservice/user/MicroserviceUserApplication.java
+++ microservice-user/src/main/java/com/microservice/user/MicroserviceUserApplication.java
@@ -2,10 +2,12 @@ package com.microservice.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class MicroserviceUserApplication {
 
 	public static void main(String[] args) {
