edafefd47500cf787595e64954dc84a4f1e230de
Brandon FT <Brandon_ft10@outlook.com>
2024-12-12 13:26:12 -0600

    Modificacion Gateway

--- microservice-user/src/main/java/com/microservice/user/model/Bike.java
+++ microservice-user/src/main/java/com/microservice/user/model/Bike.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Bike {
@@ -12,5 +11,27 @@ public class Bike {
     private String brand;
     private String model;
     private int userId;
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+    
+    
+    
+    
 
 }
--- microservice-user/src/main/java/com/microservice/user/model/Car.java
+++ microservice-user/src/main/java/com/microservice/user/model/Car.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Car {
@@ -12,4 +11,25 @@ public class Car {
     private String brand;
     private String model;
     private int userId;
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+    
+    
+    
 }
